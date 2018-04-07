d8602fa66e804793c34ecb67d16a4540d163bf33
boobae <egbunaoluebube@gmail.com>
2018-04-07 07:22:28 +0100

    axios imported

--- src/GitForm.js
+++ src/GitForm.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
+import axios from 'axios';
+
+
+
 class GitForm extends React.Component{
-	
-	
+
+	state = { userName :'' }
+
 	newDataSubmit=(event) => {
 		event.preventDefault();
 
-		console.log('Event: submit', this.userName.value);
+		console.log('Event: submit', this.state.userName);
+
+		axios.get(`https://api.github.com/users/${this.state.userName}`)
+		.then(response=>{
+			console.log(response);
+		});
 	};
 
 	render(){
 		return(
 			<form onSubmit={this.newDataSubmit}>
-				<input type="text" ref={(newInput)=>this.userName=newInput}  placeholder="GitHub userName" required/>
+				<input 
+				type="text" 
+				value={this.state.userName}  
+				onChange={(event) => this.setState({userName: event.target.value})} 
+				placeholder="GitHub userName" required/>
+
 				<button type="submit">Add</button>
 			</form>
 
